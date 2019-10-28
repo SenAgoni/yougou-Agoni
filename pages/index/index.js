3a308a3944dbcf1542b155be515dffddfedd0af0
SenAgoni <Agoni@740657284@163.com>
2019-10-28 19:39:57 +0800

    头部部分与轮播图样式完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,21 @@
 //index.js
 import request from '../../utils/request.js'
 Page({
- onLoad(){
- }
+
+  data:{
+    swiperData:[],
+  },
+  onLoad(){
+    // 轮播图请求图片数据
+    request({
+      url:"/api/public/v1/home/swiperdata"
+    }).then(res=>{
+      const {message} = res.data;
+      if(res.data.meta.status===200){
+        this.setData({
+          swiperData: message,
+        })
+      }
+    })
+  }
 })
